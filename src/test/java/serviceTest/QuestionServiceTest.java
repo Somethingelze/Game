4f89b406f0d07d5f5b24aaa1ba3067d615677f09
some <xxvikingxx@yandex.ru>
2024-07-10 23:13:56 +0300

    first

--- src/test/java/serviceTest/QuestionServiceTest.java
+++ src/test/java/serviceTest/QuestionServiceTest.java
@@ -1,34 +1,20 @@
-package service;
+package serviceTest;
 
 import model.Data;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+import service.QuestionService;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@ExtendWith(MockitoExtension.class)
 public class QuestionServiceTest {
-
-    @Mock
-    QuestionService mockQuestionService;
-
     @Test
     public void testReadFromFile() throws IOException, URISyntaxException {
         QuestionService questionService = new QuestionService();
 
-            Data data = questionService.readFromFile();
-            assertNotNull(data);
+        Data data = questionService.readFromFile();
+        assertNotNull(data);
     }
-
-    @Test
-public void test()  {
-
-
-    }
-    }
+}
